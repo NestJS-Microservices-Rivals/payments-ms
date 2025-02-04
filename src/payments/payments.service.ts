c1f00fa4c5e88951f56b5e6957f8eaafcac1ddcb
yvromero <yvonne.romero@uds.edu.py>
2025-02-04 19:47:18 -0300

    Update payments: improvements in configuration, services, and Docker

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,12 +1,20 @@
-import { Injectable } from '@nestjs/common';
-import { envs } from 'src/config';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { envs, NATS_SERVICE } from 'src/config';
 import Stripe from 'stripe';
 import { PaymentSessionDto } from './dto/payment-session.dto';
 import { Request, Response } from 'express';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.stripeSecret);
+  private readonly logger = new Logger('PaymentsService');
+
+  constructor(
+    @Inject(NATS_SERVICE) private readonly client: ClientProxy
+  ) {
+
+  }
 
   async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
     const { currency, items, orderId } = paymentSessionDto;
@@ -38,7 +46,12 @@ export class PaymentsService {
       cancel_url: envs.stripeCancelUrl,
     });
 
-    return session;
+    //return session;
+    return {
+      cancelUrl: session.cancel_url,
+      successUrl: session.success_url,
+      url: session.url,
+    }
   }
 
   async stripeWebhook( req: Request, res: Response ) {
@@ -56,13 +69,15 @@ export class PaymentsService {
     //console.log({event});
     switch( event.type ) {
       case 'charge.succeeded':
-        const chargeSucceded = event.data.object;
-        // TODO llamar nuestro microservice
 
-        console.log({
-          metadata: chargeSucceded.metadata,
-          orderId: chargeSucceded.metadata.orderId
-        }) ;
+        const chargeSucceded = event.data.object;
+        const payload = {
+          stripePaymentId: chargeSucceded.id,
+          orderId: chargeSucceded.metadata.orderId,
+          receiptUrl: chargeSucceded.receipt_url,
+        }
+
+        this.client.emit('payment.succeeded', payload );
 
       break;
 
